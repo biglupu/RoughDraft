d003095d9550fc567b0892ccb48ca9c341d09c59
Jonne Välimäki <jonne.valimaki@tuni.fi>
2022-08-28 21:52:08 +0300

    28.8 Progress

--- main.cpp
+++ main.cpp
@@ -8,18 +8,30 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
+
+	try {
+		if (argc > 4) {
+			throw argc;
+		}
+	}
+	catch (int x) {
+		cout << "\nToo many arguments(" << x << "), seizing operations";
+		return 0;
+	}
+
+
 	if (argc == 1)
 		doingStuff();
 	else if (argc == 2)
 		doingStuff(argc, argv[1]);
 	else if (argc == 3)
-		doingStuff(argc, argv[1], argv[2]); 
+		doingStuff(argc, argv[1], argv[2]);
 	else if (argc == 4)
-		doingStuff(argc, argv[1], argv[2], argv[3]); 
-	else if (argc > 4)
-	{
-		cout << "\nToo many arguments, seizing operations";
-	}
+		doingStuff(argc, argv[1], argv[2], argv[3]);
+
+
+
+
 
 	return 0;
 }
